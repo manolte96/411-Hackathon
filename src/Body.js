86849979df8ba82efc6282f995632d5f04dc19cc
manolte96 <michaelnolte13@gmail.com>
2022-08-16 23:35:28 -0500

    90% stylized

--- src/Body.js
+++ src/Body.js
@@ -5,32 +5,6 @@ export default function Body(props) {
   console.log("props", props);
 
   return (
-<<<<<<< HEAD
-    <div>
-        <h2>Search </h2>
-        {props.list.map((item, index) => {
-            return (
-                <div key={index}>
-                    <div className='TitleContainer'>
-                    <h5> <a href = {item.url}>
-                    {item.title}
-                    </a>
-                    </h5>
-                    <h6 className='Links'> ({item.url}) </h6>
-                </div>
-                <div className='StoryContainer'>
-                    <h6>
-                       <span> {item.points} points|</span>                       
-                       <span> {item.author}|</span>
-                       <span> {item.created_at}| </span>
-                       <span> {item.num_comments} comments| </span>
-                    </h6>
-                    </div>
-                </div>
-
-            )
-        })}
-=======
     <div className="app-container">
       {props.list.map((item, index) => {
         const dateToStr = item.created_at.toString();
@@ -62,7 +36,6 @@ export default function Body(props) {
           </div>
         );
       })}
->>>>>>> 51972858d80b88ffc6266f673ba347f61ab7b754
     </div>
   );
 }
